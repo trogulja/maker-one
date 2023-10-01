a2de2b7bd5a94634900bc9dab52b8b29df5962cd
Tibor Rogulja <tibor.rogulja@productive.io>
2023-10-02 00:19:39 +0200

    extract formatTable

--- src/actions/list.ts
+++ src/actions/list.ts
@@ -1,40 +1,18 @@
 import input from '@lib/input';
 import sheetsapi from '@lib/sheets';
+import { formatTable } from '@lib/utility';
 
 export default async function list() {
-  const amount = await input.projectsAmount();
-  const projects = await sheetsapi.getProjects();
+  const [amount, projects] = await Promise.all([
+    input.projectsAmount(),
+    sheetsapi.getProjects(),
+  ]);
 
   if (!projects) return console.log('No projects found.');
 
   const slicedProjects = projects.length < amount ? projects : projects.slice(-amount);
-  const rowsMap: Record<number, number> = {};
 
-  const mappedProjects = slicedProjects.map((project) => {
-    const values = Object.values(project);
-    const keys = Object.keys(project);
-    values.forEach((value, idx) => {
-      if (!rowsMap[idx]) rowsMap[idx] = value.length;
-      rowsMap[idx] = Math.max(rowsMap[idx], value.length, keys[idx].length);
-    });
-    return values;
-  });
-
-  const result = mappedProjects
-    .map((project) => {
-      const projectString = project
-        .map((value, idx) => value.padEnd(rowsMap[idx], ' '))
-        .join(' | ');
-
-      return `  | ${projectString} |`;
-    })
-    .join('\n');
-
-  const header = Object
-    .keys(slicedProjects[0])
-    .map((key, idx) => key.padEnd(rowsMap[idx], ' '))
-    .join(' | ');
-
-  console.log(`  | ${header} |\n${result}`);
+  const result = formatTable(slicedProjects);
+  console.log(result);
   console.log();
 }
--- src/lib/utility.ts
+++ src/lib/utility.ts
@@ -11,3 +11,64 @@ export function arraysToObjects(input: string[][]) {
 
   return object;
 }
+
+export function formatDate(date: Date) {
+  const day = date.getDate().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const year = date.getFullYear().toString();
+
+  return `${day}.${month}.${year}.`;
+}
+
+export function formatFolderName({rb, date, name}: {rb: string, date: string, name: string}) {
+  const [day, month] = date.split('.').map((x) => parseInt(x).toString().padStart(2, '0'));
+  return `${rb}-${month}-${day} ${name}`;
+}
+
+export function getRb(oldRb?: string) {
+  const yy = new Date().getFullYear().toString().slice(-2);
+  let nnn = '001';
+
+  const oldNnn = parseInt(oldRb?.slice(-3) ?? '0');
+  if (oldRb && oldRb.slice(0, 2) === yy) {
+    nnn = (oldNnn + 1).toString().padStart(3, '0');
+  }
+
+  return `${yy}${nnn}`;
+}
+
+export function formatTable(data: Record<string, string>[], paddingChar = ' '): string {
+  const rowsMap: Record<number, number> = {};
+
+  const mappedData = data.map((row) => {
+    const values = Object.values(row);
+    const keys = Object.keys(row);
+    values.forEach((value, idx) => {
+      if (!rowsMap[idx]) rowsMap[idx] = value.length;
+      rowsMap[idx] = Math.max(rowsMap[idx], value.length, keys[idx].length);
+    });
+    return values;
+  });
+
+  const result = mappedData
+    .map((row) => {
+      const rowString = row
+        .map((value, idx) => value.padEnd(rowsMap[idx], paddingChar))
+        .join(' | ');
+
+      return `  | ${rowString} |`;
+    })
+    .join('\n');
+
+  const header = Object
+    .keys(data[0])
+    .map((key, idx) => key.padEnd(rowsMap[idx], paddingChar))
+    .join(' | ');
+
+  const divider = Object
+    .keys(data[0])
+    .map((_key, idx) => '-'.repeat(rowsMap[idx]))
+    .join('-:-');
+
+  return `  | ${header} |\n  :-${divider}-:\n${result}`;
+}
