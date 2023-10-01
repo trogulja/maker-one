c518f972c4730a0c11be3d8b5818c577ac109c8c
Tibor Rogulja <tibor.rogulja@productive.io>
2023-10-02 00:45:10 +0200

    copy files after sheets api

--- src/actions/create.ts
+++ src/actions/create.ts
@@ -1,6 +1,10 @@
+import path from 'path';
+import * as fs from 'fs/promises';
 import input from '@lib/input';
 import sheetsapi from '@lib/sheets';
 import { formatFolderName, formatTable } from '@lib/utility';
+import { PROJECTS_FOLDER, APP_PATH } from '@lib/consts';
+
 
 export default async function create() {
   const [name, rb] = await Promise.all([
@@ -9,17 +13,42 @@ export default async function create() {
   ]);
 
   const date = await input.projectDate();
+  const folderName = formatFolderName({rb, date, name});
 
-  const values = [[rb, date, name, formatFolderName({rb, date, name})]];
+  const values = [[rb, date, name, folderName]];
   const project = {
     Rb: rb,
     Datum: date,
     Projekt: name,
-    "Ime projekta": formatFolderName({rb, date, name}),
+    "Ime projekta": folderName,
   };
 
   await sheetsapi.addNewProject(values);
 
   console.log(formatTable([project]));
   console.log();
+
+  const folderPath = path.join(PROJECTS_FOLDER, folderName);
+
+  await fs.mkdir(path.join(folderPath, '01 RAW', 'Capture'), { recursive: true });
+  await fs.mkdir(path.join(folderPath, '01 RAW', 'Output'), { recursive: true });
+  await fs.mkdir(path.join(folderPath, '01 RAW', 'Selects'), { recursive: true });
+  await fs.mkdir(path.join(folderPath, '01 RAW', 'Trash'), { recursive: true });
+  await fs.mkdir(path.join(folderPath, '02 Radni PSD'), { recursive: true });
+  await fs.mkdir(path.join(folderPath, '03 Isporuka', folderName), { recursive: true });
+
+  await fs.copyFile(path.join(APP_PATH, 'template.cosessiondb'), path.join(folderPath, '01 RAW', `${folderName}.cosessiondb`));
+
+  // let db = new sqlite3.Database(path.join(folderPath, '01 RAW', '/' + folderName + '.cosessiondb'))
+
+  // db.serialize(function() {
+  //   let stmt = db.prepare("UPDATE ZCOLLECTION SET ZCAPTURENAMINGNAME=(?) WHERE ZNAME='root'");
+  //   stmt.run(folderName);
+  //   stmt.finalize();
+  // });
+
+  // db.close();
+
+  // await open(path.join(folderPath, '01 RAW', '/' + folderName + '.cosessiondb'), {wait: false});
+
 }
